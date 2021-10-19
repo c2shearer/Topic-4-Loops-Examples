30722c864d539d26e58be5d635273c035917482c
SHE16003894 <SHE16003894@C-320109>
2021-10-19 15:05:31 +0100

    Add project files.

---
+++ Topic 4 - Loops Examples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Topic_4___Loops_Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
